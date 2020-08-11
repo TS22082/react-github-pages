11cf0a39328218af30ab7a2e93cd4cea052ef392
TS22082 <ts22082@gmail.com>
2020-08-11 10:27:30 -0700

    Updates

---
+++ precache-manifest.5aa22bbb3b94ec8ce92003fb5a6fa812.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5207f627f522a9476df9b8b503071800",
+    "url": "/react-github-pages/index.html"
+  },
+  {
+    "revision": "03605ff8100b6a74788b",
+    "url": "/react-github-pages/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "32ea52fb4c047795af0c",
+    "url": "/react-github-pages/static/js/2.c4e3965c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-github-pages/static/js/2.c4e3965c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03605ff8100b6a74788b",
+    "url": "/react-github-pages/static/js/main.ac20efe1.chunk.js"
+  },
+  {
+    "revision": "0054ea233aedbbb4cf9d",
+    "url": "/react-github-pages/static/js/runtime-main.fc19e8b6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-github-pages/static/media/logo.5d5d9eef.svg"
+  }
+]);
